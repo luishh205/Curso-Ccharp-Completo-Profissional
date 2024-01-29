c55cbdf09b2ac5a7e5bba5d59dcdef92e64d8037
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2024-01-29 11:44:02 -0300

    Add files via upload

---
+++ Colecoes/Colecoes/Colecoes/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Colecoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnHashSet = new System.Windows.Forms.Button();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.btnSortedList = new System.Windows.Forms.Button();
+            this.btnSortedDictionary = new System.Windows.Forms.Button();
+            this.btnSortedSet = new System.Windows.Forms.Button();
+            this.btnQueue = new System.Windows.Forms.Button();
+            this.btnStack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 36;
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(678, 580);
+            this.lista.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(697, 13);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(204, 68);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnHashSet
+            // 
+            this.btnHashSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHashSet.Location = new System.Drawing.Point(697, 87);
+            this.btnHashSet.Name = "btnHashSet";
+            this.btnHashSet.Size = new System.Drawing.Size(204, 68);
+            this.btnHashSet.TabIndex = 2;
+            this.btnHashSet.Text = "HashSet";
+            this.btnHashSet.UseVisualStyleBackColor = true;
+            this.btnHashSet.Click += new System.EventHandler(this.btnHashSet_Click);
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDictionary.Location = new System.Drawing.Point(697, 161);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(204, 68);
+            this.btnDictionary.TabIndex = 3;
+            this.btnDictionary.Text = "Dictionary";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // btnSortedList
+            // 
+            this.btnSortedList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortedList.Location = new System.Drawing.Point(697, 235);
+            this.btnSortedList.Name = "btnSortedList";
+            this.btnSortedList.Size = new System.Drawing.Size(204, 68);
+            this.btnSortedList.TabIndex = 4;
+            this.btnSortedList.Text = "SortedList";
+            this.btnSortedList.UseVisualStyleBackColor = true;
+            this.btnSortedList.Click += new System.EventHandler(this.btnSortedList_Click);
+            // 
+            // btnSortedDictionary
+            // 
+            this.btnSortedDictionary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortedDictionary.Location = new System.Drawing.Point(697, 309);
+            this.btnSortedDictionary.Name = "btnSortedDictionary";
+            this.btnSortedDictionary.Size = new System.Drawing.Size(204, 68);
+            this.btnSortedDictionary.TabIndex = 5;
+            this.btnSortedDictionary.Text = "SortedDictionary";
+            this.btnSortedDictionary.UseVisualStyleBackColor = true;
+            this.btnSortedDictionary.Click += new System.EventHandler(this.btnSortedDictionary_Click);
+            // 
+            // btnSortedSet
+            // 
+            this.btnSortedSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortedSet.Location = new System.Drawing.Point(697, 383);
+            this.btnSortedSet.Name = "btnSortedSet";
+            this.btnSortedSet.Size = new System.Drawing.Size(204, 68);
+            this.btnSortedSet.TabIndex = 6;
+            this.btnSortedSet.Text = "SortedSet";
+            this.btnSortedSet.UseVisualStyleBackColor = true;
+            this.btnSortedSet.Click += new System.EventHandler(this.btnSortedSet_Click);
+            // 
+            // btnQueue
+            // 
+            this.btnQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQueue.Location = new System.Drawing.Point(697, 457);
+            this.btnQueue.Name = "btnQueue";
+            this.btnQueue.Size = new System.Drawing.Size(204, 68);
+            this.btnQueue.TabIndex = 7;
+            this.btnQueue.Text = "Queue";
+            this.btnQueue.UseVisualStyleBackColor = true;
+            this.btnQueue.Click += new System.EventHandler(this.btnQueue_Click);
+            // 
+            // btnStack
+            // 
+            this.btnStack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStack.Location = new System.Drawing.Point(697, 531);
+            this.btnStack.Name = "btnStack";
+            this.btnStack.Size = new System.Drawing.Size(204, 68);
+            this.btnStack.TabIndex = 8;
+            this.btnStack.Text = "Stack";
+            this.btnStack.UseVisualStyleBackColor = true;
+            this.btnStack.Click += new System.EventHandler(this.btnStack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 638);
+            this.Controls.Add(this.btnStack);
+            this.Controls.Add(this.btnQueue);
+            this.Controls.Add(this.btnSortedSet);
+            this.Controls.Add(this.btnSortedDictionary);
+            this.Controls.Add(this.btnSortedList);
+            this.Controls.Add(this.btnDictionary);
+            this.Controls.Add(this.btnHashSet);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coleções Genericas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnHashSet;
+        private System.Windows.Forms.Button btnDictionary;
+        private System.Windows.Forms.Button btnSortedList;
+        private System.Windows.Forms.Button btnSortedDictionary;
+        private System.Windows.Forms.Button btnSortedSet;
+        private System.Windows.Forms.Button btnQueue;
+        private System.Windows.Forms.Button btnStack;
+    }
+}
+
---
+++ Colecoes/Colecoes/Colecoes/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // Array
+            string[] nomes = new string[3];
+            nomes[0] = "Gabriel";
+            nomes[1] = "Arthur";
+            nomes[2] = "Danny";
+
+            // Lista Generica
+            List<string> nomes2 = new List<string>();
+
+            nomes2.Add("Lucas");
+            nomes2.Add("Beatriz");
+            nomes2.AddRange(nomes); 
+
+            //if (nomes2.Remove("Lucas"))
+            //{
+            //    MessageBox.Show("Lucas removido");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não foi possivel remover.");
+            //}
+
+            //if (nomes2.Contains("Danny"))
+            //{
+            //    MessageBox.Show("Contém");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não contém");
+            //}
+
+            //MessageBox.Show("Número de elementos: " + nomes2.Count);
+
+            //nomes2.Sort();
+
+            //MessageBox.Show("Gabriel está no indice: " + nomes2.IndexOf("João"));
+
+            //nomes2.Insert(2, "João");
+
+            //nomes2.RemoveAt(1);
+
+            //nomes2.Clear();
+
+            nomes2.Add("Arthur");
+
+            foreach (string nome in nomes2)
+            {
+                lista.Items.Add(nome);
+            }
+        }
+
+        private void btnHashSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            HashSet<string> veiculos = new HashSet<string>()
+            {
+                "Carro", "Moto", "Avião", "Helicoptero", "Barco"
+            };
+
+            //MessageBox.Show(veiculos.ElementAt(2));
+
+            //if (veiculos.Add("Moto"))
+            //{
+            //    MessageBox.Show("Item adicionado");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Item não adicionado");
+            //}
+
+            //if (veiculos.Contains("Carro"))
+            //{
+            //    MessageBox.Show("Contem.");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não Contem.");
+            //}
+
+            // veiculos.Remove("");
+            // veiculos.Count;
+            // veiculos.Clear();
+            // veiculos.First();
+            // veiculos.Last();
+
+            foreach (string item in veiculos)
+            {
+                lista.Items.Add(item);
+            }
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            Dictionary<int, string> alunos = new Dictionary<int, string>()
+            {
+                { 150, "Danny" },
+                { 200, "Arthur" },
+                { 80, "Danny" }
+            };
+
+            alunos.Add(100, "Gabriel");
+
+            // alunos.ContainsKey(810);
+
+            if (alunos.ContainsValue("Arthur"))
+            {
+                MessageBox.Show("Contém o aluno");
+            }
+            else
+            {
+                MessageBox.Show("Não Contém o aluno");
+            }
+            
+
+            // alunos.Remove(80);
+            // alunos.Clear();
+
+            // KeyValuePair<int, string> primeiro = alunos.First();
+            // MessageBox.Show("Primeiro " + primeiro.Key + " " + primeiro.Value);
+            
+            // MessageBox.Show("Último " + alunos.Last().Key + " " + alunos.Last().Value);
+            
+            foreach (KeyValuePair<int, string> item in alunos)
+            {
+                lista.Items.Add(item.Key + " = " + item.Value);
+            }
+
+            //MessageBox.Show("Quantidade de alunos: " + alunos.Count);
+        }
+
+        private void btnSortedList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            SortedList<int, string> alunos = new SortedList<int, string>()
+            {
+                { 27, "Danny" },
+                { 10, "Gabriel" },
+                { 17, "Arthur" }
+            };
+
+            alunos.Add(14, "Glória");
+            //alunos.Reverse();
+
+            //alunos.Remove(10);
+            //alunos.RemoveAt(0);
+            //alunos.Count;
+            //alunos.ContainsKey(50);
+            //alunos.ContainsValue("Danny");
+
+            foreach (KeyValuePair<int,string> item in alunos)
+            {
+                lista.Items.Add(item.Key + " " + item.Value);
+            }
+        }
+
+        private void btnSortedDictionary_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            SortedDictionary<int, string> alunos = new SortedDictionary<int, string>()
+            {
+                { 4512, "Gabriel" },
+                { 1542, "Arthur" },
+                { 2145, "Danny" }
+            };
+
+            alunos.Add(5478, "Fávio");
+
+            // alunos.Remove(120);
+            // alunos.Count;
+            // alunos.Clear();
+            // alunos.ContainsKey(1542);
+            // alunos.ContainsValue("Danny");
+            // MessageBox.Show(alunos.ElementAt(0).ToString());
+
+            foreach (KeyValuePair<int,string> item in alunos.Reverse())
+            {
+                lista.Items.Add(item);
+            }
+        }
+
+        private void btnSortedSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            SortedSet<string> nomes = new SortedSet<string>()
+            {
+                "Gabriel", "Danny", "Arthur", "Flávio", "Beatriz"
+            };
+
+            if (!nomes.Add("Lucas"))
+            {
+                MessageBox.Show("Não podes repstir o valor");
+            }
+
+            //nomes.Reverse();
+            //nomes.Remove("Flávio");
+            //nomes.ElementAt(1);
+            //nomes.First();
+            //nomes.Last();
+            //nomes.Count;
+
+            //nomes.Clear();
+
+            foreach (string nome in nomes)
+            {
+                lista.Items.Add(nome);
+            }
+        }
+
+        private void btnQueue_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            Queue<string> fila = new Queue<string>();
+            fila.Enqueue("Gabriel");
+            fila.Enqueue("Danny");
+            fila.Enqueue("Arthur");
+
+            MessageBox.Show(fila.Count.ToString());
+
+            foreach (string item in fila)
+            {
+                lista.Items.Add(item);
+            }
+
+            //MessageBox.Show("Primeiro da fila " + fila.Peek());
+            //MessageBox.Show(fila.Count.ToString());
+
+            //MessageBox.Show("Primeiro da fila " + fila.Dequeue());
+            //MessageBox.Show(fila.Count.ToString());
+
+            //MessageBox.Show("Primeiro da fila " + fila.First());
+            //MessageBox.Show(fila.Count.ToString());
+
+            //MessageBox.Show("Último da fila " + fila.Last());
+            //MessageBox.Show(fila.Count.ToString());
+
+            //fila.Clear();
+            while (fila.Count > 0)
+            {
+                MessageBox.Show("Primeiro da fila " + fila.Dequeue());
+                MessageBox.Show(fila.Count.ToString());
+                lista.Items.Clear();
+                foreach (string item in fila)
+                {
+                    lista.Items.Add(item);
+                }
+            }            
+        }
+
+        private void btnStack_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            Stack<string> pilha = new Stack<string>();
+
+            pilha.Push("Gabriel");
+            pilha.Push("Danny");
+            pilha.Push("Arthur");
+
+            MessageBox.Show(pilha.Count.ToString());
+
+            foreach (string item in pilha)
+            {
+                lista.Items.Add(item);
+            }
+
+            // MessageBox.Show("Elemento no topo " + pilha.Peek());
+            // MessageBox.Show(pilha.Count.ToString());   
+
+            // pilha.Clear();
+
+            while (pilha.Count > 0)
+            {
+                MessageBox.Show("Elemento no topo " + pilha.Pop());
+                MessageBox.Show(pilha.Count.ToString());
+                lista.Items.Clear();
+                foreach (string item in pilha)
+                {
+                    lista.Items.Add(item);
+                }
+            }
+        }
+    }
+}
